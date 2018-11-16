0360b884ef021cd2e5fe0acb74b17ba1ffb44819
bfahm <bola_fah.97@hotmail.com>
2018-11-16 04:26:05 +0200

    Split omegaNMatrix to multiple functions to organize code

--- main.cpp
+++ main.cpp
@@ -1,9 +1,7 @@
 #include <iostream>
 #include <cmath>
 #define PI 3.14159265
-
 using namespace std;
-
 
 class Complex
 {
@@ -14,12 +12,10 @@ private:
 public:
     Complex(double r = 0, double i = 0): real(r), img(i) {};
 
-    double returnReal(void)
-    {
+    double returnReal(){
         return this->real;
     }
-    double returnImg(void)
-    {
+    double returnImg(){
         return this->img;
     }
     Complex add(const Complex& c)
@@ -47,22 +43,21 @@ public:
         return result;
 
     }
-    void printComplex(void)
-    {
-        if (this->img>0) {
-            if(this->img==1){
-                cout << this->real << " + j";
+    void printComplex(){
+        if(this->img==0){
+            cout<<this->real;
+        }else if(this->real==0) {
+            if(this->img<0) {
+                cout<<"-j";
+            }else if(this->img==0) {
+                cout<<"0";
             }else {
-                cout << this->real << " + j" << this->img;
+                cout<<"j";
             }
-        }else if(this->img<0){
-            if(this->img==-1){
-                cout << this->real << " - j";
-            }else {
-                cout << this->real << " - j" << this->img;
-            }
+        }else if(this->img>0){
+            cout<<this->real<<"+"<<this->img<<"j";
         }else{
-            cout << this->real;
+            cout<<this->real<<this->img<<"j";
         }
     }
 };
@@ -80,13 +75,15 @@ double calculatePhase(Complex number){
     double result = atan (param) * 180 / PI;
     if(result<0){
         result+=180;
+    }else if(result == -0){
+        result = 0;
     }
     return result;
 }
 
 Complex omegaNValue (int k, int n, int N){
     if (k*n != 0){
-        double angle = (2*180/(N))* k * n;
+        int angle = (2*180/(N))* k * n;
         double validRadian = angle*PI/180;
 
         double cosResult = cos(validRadian);
@@ -100,102 +97,101 @@ Complex omegaNValue (int k, int n, int N){
     return 1;
 }
 
-Complex omegaNMatrix (int N, int toPrint, int toMultiply){
+Complex* omegaNInflator (Complex omegaNMatrix[], int N) {
     int n = N, k = N;
-    Complex matrix[k][n];
-    for(int i = 0; i<n; i++){
-        for(int j = 0; j<k; j++){
-            matrix[i][j] = omegaNValue(i, j, N);
+    int counter = 0;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < k; j++) {
+            omegaNMatrix[counter] = omegaNValue(i, j, N);
+            counter += 1;
         }
     }
-    //-----------------------------------------
-    if(toPrint==1){
-        cout<<"Printing Wn Matrix form:"<<endl;
-        for(int i = 0; i<N; i++){
-            for(int j = 0; j<N; j++){
-                matrix[i][j].printComplex();
-                cout << "\t\t\t";
-            }
-            cout << endl;
-        }
-    }else if(toPrint==2){
-        cout<<"Printing Wn Vector form:"<<endl;
-        for(int i = 0; i<N; i++){
-            for(int j = 0; j<N; j++) {
-                matrix[i][j].printComplex();
-                cout << endl;
-            }
-        }
-    }
-    //-----------------------------------------
-    if(toMultiply==1){
-        int numOfInput = N;
-        int userInput[numOfInput];
-        cout<<"Enter X(N) values:"<<endl;
-        cout<<numOfInput<<" values needed"<<endl;
-        cout<<"Continue with ZEROS '0' if you want to enter number of Xs less than N-point Number"<<endl;
-        for (int i = 0; i<numOfInput; i++){
-            cin >> userInput[i];
-        }
-        //-------------------------------------
-        Complex multiplicationOutput[numOfInput];
-        for(int i=0; i<N;i++){
-            for(int j=0; j<N;j++){
-                multiplicationOutput[i] = multiplicationOutput[i].add(matrix[i][j].multiply(userInput[j]));
-            }
-        }
-        cout<<"Results:"<<endl;
-        cout<<"---------------------------------------------------"<<endl;
-        for(int i = 0; i<numOfInput; i++){
-            multiplicationOutput[i].printComplex();
-            cout << endl;
-        }
-        //-------------------------------------
-        cout<<"---------------------------------------------------"<<endl;
-        cout<<"Printing Magnitudes"<<endl;
-        cout<<"[";
-        for(int i = 0; i<numOfInput; i++){
-            cout <<calculateMag(multiplicationOutput[i]);
-            cout << "\t\t";
-        }
-        cout << "]" << endl;
-        cout<<"Printing Phases"<<endl;
-        cout<<"[";
-        for(int i = 0; i<numOfInput; i++){
-            cout <<calculatePhase(multiplicationOutput[i]);
-            cout << "\t";
-        }
-        cout << "]" << endl;
-    }
-    return matrix[k][n];
+    return omegaNMatrix;
 }
 
+Complex* calculateDFT (Complex omegaNMatrix[],Complex multiplicationOutput[], int N) {
+    int userInput[N];
+    cout << endl << "Enter X(N) values:" << endl;
+    cout << N << " values needed" << endl;
+    cout << "Continue with ZEROS '0' if you want to enter number of Xs less than N-point Number" << endl;
+    for (int i = 0; i < N; i++) {
+        cin >> userInput[i];
+    }
+    cout << "Successfully Entered x(N) values." << endl;
+    //-------------------------------------
+    int multiplier = 0;
+    Complex singleRowResult = 0;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            singleRowResult = singleRowResult.add(omegaNMatrix[j + multiplier].multiply(userInput[j]));
+        }
+        multiplicationOutput[i] = singleRowResult;
+        singleRowResult = 0;
+        multiplier += N;
+    }
+    return multiplicationOutput;
+}
 
+void printOmegaN(Complex matrix[], int N) {
+    cout << endl << "Printing Wn:" << endl;
+    int offset = 0;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            matrix[j+offset].printComplex();
+            cout << "\t\t\t";
+        }
+        offset+=N;
+        cout << endl;
+    }
+}
 
+void printResults(Complex matrix[], int N) {
+    cout << "---------------------------------------------------" << endl;
+    cout << "Printing Results:" << endl << endl;
+
+    cout << "X(N) = [";
+    for (int i = 0; i < N; i++) {
+        matrix[i].printComplex();
+        if(i!=(N-1)) {
+            cout << ", ";
+        }
+    }
+    cout << "]" << endl;
+
+    cout<<"Magnitudes = [";
+    for(int i = 0; i<N; i++){
+        cout <<calculateMag(matrix[i]);
+        if(i!=(N-1)) {
+            cout << ", ";
+        }
+    }
+    cout << "]" << endl;
+
+    cout<<"Phases = [";
+    for(int i = 0; i<N; i++){
+        cout <<calculatePhase(matrix[i]);
+        if(i!=(N-1)) {
+            cout << ", ";
+        }
+    }
+    cout << "]" << endl;
+}
 
 int main()
 {
+    cout<<"DFT Calculator"<<endl<<endl;
+
     cout << "Enter the number of N-Points:"<<endl;
     int N;
     cin >>  N;
-    cout << "Do you want to show the Omega 'Wn' matrix while calculations occur?"<<endl;
-    cout << "Type: \n0 for no, \n1 for yes - matrix form,\n2 for yes - vector form"<<endl;
-    int toPrint;
-    cin >> toPrint;
-    if(toPrint!=0 && toPrint!=1 && toPrint!=2){
-        cout << "Error: Wrong input type."<<endl;
-        exit(0);
-    }
-    cout << "Do you want to show the final result (disable for testing purposes)?"<<endl;
-    cout << "Type 0 for no, 1 for yes"<<endl;
-    int toResult;
-    cin >> toResult;
-    if(toResult!=0 && toResult!=1){
-        cout << "Error: Wrong input type."<<endl;
-        exit(0);
-    }
-    cout << "-----------------------------------------------------------------" << endl;
-    omegaNMatrix(N, toPrint, toResult);
 
+    Complex omegaNHolder[N*N];
+    Complex outputHolder[N];
 
+    Complex* omegaNMatrix = omegaNInflator(omegaNHolder, N);
+    printOmegaN(omegaNMatrix, N);
+    Complex* results = calculateDFT(omegaNMatrix, outputHolder, N);
+    printResults(results, N);
 }
+
